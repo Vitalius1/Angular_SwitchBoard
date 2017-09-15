f191b196c835d6c8125c7a6a07b2e651827bb8ea
VitalieBraga <vitaliebraga@gmail.com>
2017-09-15 13:21:03 -0700

    Finished the switchboard assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'SwitchBoard';
+  buttons = [
+    {
+      on: false,
+      label: "OFF"
+    },
+    {
+      on: true,
+      label: "ON"
+    },
+    {
+      on: true,
+      label: "ON"
+    },
+    {
+      on: false,
+      label: "OFF"
+    },
+    {
+      on: true,
+      label: "ON"
+    },
+    {
+      on: true,
+      label: "ON"
+    },
+    {
+      on: true,
+      label: "ON"
+    },
+    {
+      on: false,
+      label: "OFF"
+    },
+    {
+      on: true,
+      label: "ON"
+    },
+    {
+      on: true,
+      label: "ON"
+    },
+  ];
+  switch(idx) {
+    if (this.buttons[idx].on) {
+      this.buttons[idx].on = false;
+      this.buttons[idx].label = "OFF";
+    } else {
+      this.buttons[idx].on = true
+      this.buttons[idx].label = "ON"
+    }
+  }
 }
